0de21a3324d36f8bae40615263eaa92a7ce056d8
manmanlan <prikitiki860@gmail.com>
2025-08-20 13:14:27 +0200

    aaa

---
+++ manen/player_battle.gd
@@ -0,0 +1,13 @@
+extends Sprite2D
+
+
+
+
+
+
+func _on_button_pressed() -> void:
+	$AnimationPlayer.play("default attack")
+
+
+func _on_defend_pressed() -> void:
+	pass # Replace with function body.
