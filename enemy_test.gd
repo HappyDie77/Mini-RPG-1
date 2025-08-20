2e6c52c452baa38f8bce6e2a9512698fcdfa3751
manmanlan <prikitiki860@gmail.com>
2025-08-20 12:06:49 +0200

    woooooooooyippppee yiiiiiipppe

--- enemy_test.gd
+++ enemy_test.gd
@@ -1,17 +1,19 @@
-#extends CharacterBody2D
- #
- #
-#const SPEED = 100.0
- #
- #
-#func _physics_process(delta: float) -> void:
-	## Get direction from this enemy to the player
-	#var direction = (Global.player_pos - global_position).normalized()
-	## Move towards the player
-	#velocity = direction * SPEED
-	#move_and_slide()
- #
- #
-#func _on_area_2d_body_entered(body: Node2D) -> void:
-	#if body.is_in_group("player"):
-		#get_tree().paused=true
+extends CharacterBody2D
+ 
+ 
+const SPEED = 100.0
+ 
+ 
+func _physics_process(delta: float) -> void:
+	# Get direction from this enemy to the player
+	var direction = (Global.player_pos - global_position).normalized()
+	# Move towards the player
+	velocity = direction * SPEED
+	move_and_slide()
+ 
+
+
+
+func _on_hitbox_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		get_tree().paused=true
