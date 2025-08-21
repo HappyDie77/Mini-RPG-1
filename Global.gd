95cbcb923aaecee8482877222a54a0668f7a9279
HappyDie77 <92266147+HappyDie77@users.noreply.github.com>
2025-08-21 11:27:37 +0200

    dtyuio

--- Global.gd
+++ Global.gd
@@ -1,5 +1,6 @@
 extends Node
 
+<<<<<<< Updated upstream
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,3 +10,8 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+=======
+var player_pos
+
+var mana = 0
+>>>>>>> Stashed changes
--- enemy_test.gd
+++ enemy_test.gd
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 #extends CharacterBody2D
  #
  #
@@ -15,3 +16,25 @@
 #func _on_area_2d_body_entered(body: Node2D) -> void:
 	#if body.is_in_group("player"):
 		#get_tree().paused=true
+=======
+extends CharacterBody2D
+ 
+ 
+const SPEED = 100.0
+ 
+ 
+func _physics_process(delta: float) -> void:
+	# Get direction from this enemy to the player
+	if Global.player_pos !=null:
+		var direction = (Global.player_pos - global_position).normalized()
+		# Move towards the player
+		velocity = direction * SPEED
+	move_and_slide()
+ 
+
+
+
+func _on_hitbox_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		get_tree().paused=true
+>>>>>>> Stashed changes
